9597a302d55193d291731a740e39d7683756fd27
Kurayomi <hsjsjdbxism@gmail.com>
2025-10-06 13:55:46 +0300

    Лабораторная 5

---
+++ lb_5/lb_5.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	
+}
+
